019ed351f8da6efc8045c69f48e165cd60b50d54
patunia <pborowiec@alumni.nd.edu>
2018-07-31 13:47:47 -0500

    loose ends

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var exphbs = require('express-handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+var app = express();
+
 var PORT = process.env.PORT || 8080;
 
 var app = express();
@@ -15,8 +21,6 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
