2c510f9a8e89d0e810de84fcc7cb04c593c66bd2
patunia <pborowiec@alumni.nd.edu>
2018-07-31 11:10:16 -0500

    initial build with db and dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Montana Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jello Burger', false);
